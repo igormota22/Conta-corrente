a60cb0df908a7cc4bdcae26e0ba22b86c15b97b4
Igor Mota <igormello399@gmail.com>
2026-04-06 14:21:39 -0300

    Criada v1 e feita logica inicial

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Atributos
+
+ID de identificação
+titular
+saldo disponivel
+limite de debito
+
+Acoes da conta
+
+saque
+deposito
+consulta de saldo
+*/
+
+using System.Security.Cryptography;
+
+string titular = "Igor";
+int numeroIdentificacao = RandomNumberGenerator.GetInt32(1, 101);
+decimal saldo = 1000;
+decimal limiteDebito = 1200;
+while (true)
+{
+    Console.Clear();
+    System.Console.WriteLine("--------------------------------");
+    System.Console.WriteLine($"Conta Corrente {numeroIdentificacao} de {titular}");
+    System.Console.WriteLine("--------------------------------");
+    System.Console.WriteLine("1 - Saque");
+    System.Console.WriteLine("2 - Deposito");
+    System.Console.WriteLine("3 - Consultar Saldo");
+    System.Console.WriteLine("S - Sair");
+    System.Console.Write("Escolha a operação desejada:");
+    string? opcaoMenu = Console.ReadLine()?.ToUpper();
+
+    if (opcaoMenu == "S")
+    {
+        break;
+    }
+
+    switch (opcaoMenu)
+    {
+        case "1":
+            System.Console.Write("Informe o valor que deseja sacar:");
+            decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
+
+            if (saldo <= -limiteDebito)
+            {
+                System.Console.WriteLine("O valor de limite de debito ja foi ultrapassado");
+                Console.ReadLine();
+                return;
+            }
+            else
+            {
+
+                saldo -= valorSaque;
+                System.Console.WriteLine("O valor foi sacado com sucesso");
+                Console.ReadLine();
+            }
+            break;
+
+        case "2":
+            System.Console.Write("Informe o valor que deseja depositar:");
+            decimal valorDeposito = Convert.ToDecimal(Console.ReadLine());
+
+            saldo += valorDeposito;
+            break;
+
+        case "3":
+            System.Console.WriteLine($"Saldo: R${saldo}");
+            Console.ReadLine();
+            break;
+
+        default:
+            System.Console.WriteLine("Informe uma operação valida");
+            return;
+    }
+
+
+
+
+}
